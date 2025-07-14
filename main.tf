fcb85481d6372dfc2608cc3b2de5af776477c5e0
aditya-raj-112 <braininsight9@gmail.com>
2025-07-14 21:41:45 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred AWS region
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-unique-bucket-nameasa-123456"  # Change to a globally unique name
+
+  tags = {
+    Name        = "ExampleBucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
