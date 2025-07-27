006a511e5e8a3f41f5509cc2f433d3f6ab01d67b
aditya-raj-112 <braininsight9@gmail.com>
2025-07-27 17:30:15 +0000

    Update main.tf

--- Terra7/main.tf
+++ Terra7/main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-08a6efd148b1f7504"  # Amazon Linux 2 AMI (update as needed)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
 
