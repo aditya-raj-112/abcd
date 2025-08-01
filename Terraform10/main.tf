b17f423f3cf53171dbdf9da1800ae772dd977d3a
aditya-raj-112 <braininsight9@gmail.com>
2025-08-01 08:03:52 +0000

    Update main.tf

--- Terraform10/main.tf
+++ Terraform10/main.tf
@@ -6,6 +6,6 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-github-actions-s3-bucket"
   tags = {
     Name        = "GitHubActionsBucket"
-    Environment = "Dev"
+    Environment = "Dev1"
   }
 }
